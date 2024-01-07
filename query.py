de48f0be75aa5a6c27bf15ec8845f7987d1c0109
Kostiantyn <zvieriev.kp31.ms@gmail.com>
2024-01-07 20:13:26 +0200

    *: implement query module

--- query.py
+++ query.py
@@ -2,9 +2,18 @@ from indexfile import remove_punctuation
 
 def query_index(query_word, index, filenames):
     query_word = remove_punctuation(query_word)
-    
     if query_word not in index:
         return f'The word "{query_word}" is not found in any of the indexed files.', 404
 
-    result = 'frequency filename\n'
-    return result, 200
+    result = []
+    for k, v in enumerate(index[query_word]):
+        if v != 0:
+            result.append({"value": v, "filename": filenames[k]})
+    
+    result = sorted(result, key=lambda d: d['value'], reverse=True)
+    res_str = ''
+    for entry in result:
+        filename = entry['filename']
+        frequency = entry['value']
+        res_str += f'{frequency} {filename}\n'
+    return res_str, 200
