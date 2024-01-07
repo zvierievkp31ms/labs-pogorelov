a12930cb02bfbdc7c056ae36c3b18ffeaafe6b74
Kostiantyn <zvieriev.kp31.ms@gmail.com>
2024-01-07 19:42:07 +0200

    +: prototype for indexing file module

---
+++ indexfile.py
@@ -0,0 +1,15 @@
+import json
+import string
+from linked_list import LinkedList
+
+def remove_punctuation(word):
+    return word
+
+def index_file(files):
+    wordList = LinkedList(len(files))
+
+    with open('index', 'w', encoding='utf-8') as index_file:
+        json.dump(wordList.toObj(), index_file, indent=2)
+
+    with open('filenames', 'w', encoding='utf-8') as filenames_file:
+        json.dump(list(files.keys()), filenames_file, indent=2)
