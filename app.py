34e2ce3d0c5328770fe5b0981ca60ba2c65d1928
Kostiantyn <zvieriev.kp31.ms@gmail.com>
2024-01-08 18:14:43 +0200

    *: implement prototype changes

--- app.py
+++ app.py
@@ -1,68 +1,61 @@
 import os
 import json
-from zipfile import ZipFile
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 
 from query import query_index
 from indexfile import index_file
 
-
 app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
+dir_path = 'input_files'
+result_dir = 'results/'
 
 @app.route('/')
 def index():
-    return render_template('index.html'), 200
+    sub_dir_names = os.listdir(dir_path)
+    dir_options = {}
+    for sub_dir in sub_dir_names:
+        files = os.listdir(dir_path + '/' + sub_dir)
+        dir_options[sub_dir] = files
+    f = open('result.json')
+    index_options = json.load(f)
+    f.close()
+    return render_template('index.html', dir_options=dir_options, index_options=index_options), 200
+
+@app.route('/input', methods=['POST'])
+def input_part():
+    try:
+        data = request.get_json()
+        index_filename = data['index']
+        filenames_filename = data['filenames']
+        dir_name = data['dir']
+        files = os.listdir(dir_path + '/' + dir_name)
+        files = [ dir_path + '/' + dir_name + '/' + f for f in files]
+        index_file(files, result_dir + index_filename, result_dir + filenames_filename)
+        return 'changes applied', 200
+    except Exception as e:
+        return f"Error: The application cannot process this request, please check the data entered.", 400
 
 @app.route('/query', methods=['GET'])
 def search():
     try:
+        index_filename = request.args.get('index')
+        filenames_filename = request.args.get('filenames')
+        with open('result.json', 'r', encoding='utf-8') as result_file:
+            data = json.load(result_file)
+
+        if index_filename not in data:
+            return "Error: Not correct index file name, value not exist", 400
+        if data[index_filename] != filenames_filename:
+            return "Error: Not correct filenames file name", 400
+        
         word = request.args.get('value')
-        with open('index', 'r', encoding='utf-8') as index_file:
+        with open(index_filename, 'r', encoding='utf-8') as index_file:
             index = json.load(index_file)
-        with open('filenames', 'r', encoding='utf-8') as filenames_file:
+        with open(filenames_filename, 'r', encoding='utf-8') as filenames_file:
             filenames = json.load(filenames_file)
         return query_index(word, index, filenames)
-
     except json.JSONDecodeError:
         return f"Error: JSON files index or filenames decoding problem.", 400
-    except:
-        return f"Error: The application cannot process this request, please check the data entered.", 400
-
-@app.route('/upload', methods=['POST'])
-def upload_files():
-    try:
-        files = request.files.getlist('file')
-        if not files:
-            return 'No files selected', 400
-
-        res_files = {}
-        for file in files:
-            filename = file.filename
-            content = file.read().decode('UTF-8')
-            res_files[filename] = content
-        
-        index_file(res_files)
-        file_paths = ['index', 'filenames']
-        zip_filename = 'index_filenames.zip'
-        with ZipFile(zip_filename, 'w') as zip_file:
-            for file_path in file_paths:
-                zip_file.write(file_path, arcname=os.path.basename(file_path))
-        return send_file(zip_filename, as_attachment=True), 200
-    except:
-        return f"Error: The application cannot process this request, please check the data entered.", 400
-    
-
-@app.route('/index_filenames', methods=['POST'])
-def upload_config_files():
-    try:
-        files = request.files.getlist('file')
-        if not files:
-            return 'No files selected', 400
-
-        for file in files:
-            with open(file.filename, 'w', encoding='utf-8') as new_file:
-                json.dump(json.loads(file.read().decode('UTF-8')), new_file, indent=2)
-        return 'changes applied', 200
     except:
         return f"Error: The application cannot process this request, please check the data entered.", 400
 
